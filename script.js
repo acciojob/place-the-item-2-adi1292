050e38bcc80b85f16c73e7d316133c5d8e8ad55c
Aditya Jejurkar <159719227+adi1292@users.noreply.github.com>
2025-12-03 14:56:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+.holder {
+    display: flex;
+    flex-direction: row;      
+    justify-content: center;  
+    align-items: center;     
+    flex-wrap: wrap;          /* If more items are added, they move to next row */
+}
+
+.item {
+    width: 100px;
+    height: 100px;
+    color: white;
+    font-size: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.item:nth-child(1) {
+    background: green;
+}
+
+.item:nth-child(2) {
+    background: red;
+}
+
+.item:nth-child(3) {
+    background: blue;
+}
+
